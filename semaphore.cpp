f26a0a4deb0446bfa5f5e2a6a37fc6980687dbe8
Dizan Vasquez <dix_ans@fastmail.fm>
2013-12-12 17:40:28 +0100

    Still working on semaphore

--- semaphore.cpp
+++ semaphore.cpp
@@ -6,22 +6,10 @@
 #include <string.h>
 #include <errno.h>
 
-//struct semun {
-  //int val;
-  //struct semid_ds * buf;
-  //unsigned short int * array;
-  //struct seminfo * __buf;
-//};
-
 Semaphore::Semaphore( int key )
 {
   id_ = semget ( key, 1, IPC_CREAT | 0777 );
   std::cerr << "Created semaphore, return code:" << id_ << std::endl;
-  //semun argument;
-  //unsigned short values[1];
-  //values[0] = 1;
-  //argument.array = values;
-  //int result = semctl( id_, 0, SETALL, argument );
   int result = semctl( id_, 0, SETALL, 1 );
   if ( result != 0 ) {
     std::cerr << "Problem initializing semaphore:" << strerror( errno ) << std::endl;
